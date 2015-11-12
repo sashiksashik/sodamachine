f253827ff48d08e792b1cffcd0d2af2af499f904
Alexander Ostroumov <alexanderostroumov@yandex.ru>
2015-11-12 13:07:21 +0300

    Добавил классы Wallet, Coin. Добавил тест для Wallet.

---
+++ SodaMachine/Wallet.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodaMachine
+{
+    /// <summary>
+    /// Represents wallet
+    /// </summary>
+    public class Wallet
+    {
+
+        private List<Coin> coins;
+        /// <summary>
+        /// Initialize wallet
+        /// </summary>
+        /// <param name="centCount">1 cent pcs</param>
+        /// <param name="cent5Count">5 cent pcs</param>
+        /// <param name="cent25Count">25 cent pcs</param>
+        /// <param name="cent50Count">50 cent pcs</param>
+        public Wallet(uint centCount, uint cent5Count, uint cent25Count, uint cent50Count)
+        {
+            coins = new List<Coin>();
+            coins.Add(new Coin(1, centCount));
+            coins.Add(new Coin(5, cent5Count));
+            coins.Add(new Coin(25, cent25Count));
+            coins.Add(new Coin(50, cent50Count));
+        }
+        /// <summary>
+        /// Calculate coins amount in wallet
+        /// </summary>
+        /// <returns>Coins amount</returns>
+        public uint getAmount()
+        {
+            uint amount = 0;
+            coins.ForEach(coin => amount += coin.nominal * coin.count);
+            return amount;
+        }
+    }
+    /// <summary>
+    /// Represents coin in wallet
+    /// </summary>
+    public class Coin
+    {
+        public uint nominal { get; }
+        public uint count { get; set; }
+        public Coin() { }
+        public Coin(uint nominal, uint count)
+        {
+            this.nominal = nominal;
+            this.count = count;
+        }
+    }
+}
---
+++ TestSodaMchine/WalletTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SodaMachine;
+
+namespace TestSodaMchine
+{
+    [TestClass]
+    public class WalletTest
+    {
+        [TestMethod]
+        public void CorrectAmountCalculating()
+        {
+            Wallet wallet = new Wallet(1, 2, 3, 4);
+            Assert.AreEqual(286, wallet.getAmount());
+            Wallet secondWallet = new Wallet(1, 0, 1, 1);
+            Assert.AreEqual(76, secondWallet.getAmount());
+            Wallet thirdWallet = new Wallet(0, 0, 0, 0);
+            Assert.AreEqual(0, thirdWallet.getAmount());
+        }
+    }
+}
